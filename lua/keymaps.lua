ba6d1331d87fad6807f18b0d4e71bd6bcdcebcb7
zodiepupper <zodywoolsey@users.noreply.github.com>
2024-05-07 05:36:52 -0600

    init

---
+++ lua/keymaps.lua
@@ -0,0 +1,9 @@
+local opts = {
+	noremap = true,
+	silent = true
+}
+vim.keymap.set('t','ESC','<C-\\><C-n>',opts)
+vim.keymap.set('n','<C-k>',':resize -2<CR>',opts)
+vim.keymap.set('n','<C-j>',':resize +2<CR>',opts)
+vim.keymap.set('n','<C-h>',':vertical resize -2<CR>',opts)
+vim.keymap.set('n','<C-l>',':vertical resize +2<CR>',opts)
---
+++ lua/options.lua
@@ -0,0 +1,10 @@
+-- UI CONFIG --
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+
+-- TAB STUFF --
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
